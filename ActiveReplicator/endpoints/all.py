aab734497302b3540456a9c35d37a6a96150d44b
NickGalindo <ngalindoc@unal.edu.co>
2023-11-11 05:38:30 -0500

    sownoind

--- ActiveReplicator/endpoints/all.py
+++ ActiveReplicator/endpoints/all.py
@@ -6,4 +6,4 @@ router = APIRouter()
 
 @router.post("/runShit")
 async def runShit(request: Request, shit: str):
-    return updates.run(request.app.state.redundantConnectionPool, request.app.state.baseConnectionPool, shit)
+    return await updates.run(request.app.state.redundantConnectionPool, request.app.state.baseConnectionPool, shit)
